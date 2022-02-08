17f57c6a02cbaff2399f04857f65e007066f2650
Dean Truong <25422254+dtruong8@users.noreply.github.com>
2022-02-08 14:05:16 -0800

    optimizing template cache

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"myapp/pkg/config"
 	"myapp/pkg/handlers"
+	"myapp/pkg/render"
 	"net/http"
 )
 
@@ -10,9 +13,23 @@ const portNumber = ":8080"
 
 // main is the main application function
 func main() {
+	var app config.AppConfig
 
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Cannot create template cache.")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
